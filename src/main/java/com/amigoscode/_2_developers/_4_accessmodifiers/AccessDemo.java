56082e2522f1061d5d46f602f28f7a1dce1d9ddd
Papa Macoura SAMBA <pamacoura@gmail.com>
2026-09-12 10:39:41 +0000

    Java developper access modifiers exercises

--- src/main/java/com/amigoscode/_2_developers/_4_accessmodifiers/AccessDemo.java
+++ src/main/java/com/amigoscode/_2_developers/_4_accessmodifiers/AccessDemo.java
@@ -10,25 +10,34 @@ public class AccessDemo {
 
     public static void main(String[] args) {
         // TODO: 1 - Create an instance of AccessModifiers.
+        AccessModifiers accessModifiers=new AccessModifiers();
 
         System.out.println("=== Calling Public Method ===");
         // TODO: 2 - Call the publicGreeting() method on your instance and print the result.
         //  This works because public methods are accessible from anywhere.
+        System.out.println(accessModifiers.publicGreeting());
 
         System.out.println("\n=== Calling Package-Private Method ===");
         // TODO: 3 - Call the packageGreeting() method on your instance and print the result.
         //  This works because AccessDemo is in the SAME package as AccessModifiers.
+        System.out.println(accessModifiers.packageGreeting());
 
         System.out.println("\n=== Accessing Private Field Through Getter ===");
         // TODO: 4 - Use setSecret() to set a value, then use getSecret() to read it back.
         //  Print the result. This demonstrates proper encapsulation:
         //  we access the private field only through public getter/setter methods.
+        accessModifiers.setSecret("Papa Macoura SAMBA");
+        System.out.println(accessModifiers.getSecret());
 
         System.out.println("\n=== Why Direct Private Access Won't Work ===");
         // TODO: 5 - Write a comment explaining why the following line would NOT compile:
         //  // instance.secret = "trying direct access";
         //  Then leave the line commented out, preceded by your explanation.
         //  Hint: think about what 'private' means for field visibility outside the class.
+
+        //  accessModifiers.secret won't compile because secret is private and is accessible only inside
+        //  AccessModifiers class
+        //  accessModifiers.secret = "trying direct access";
 
     }
 }
